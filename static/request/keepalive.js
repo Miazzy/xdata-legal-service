573e46d5a8b5d533d6e417615a885ea158aa4729
yunwisdom <qq695926920@163.com>
2021-04-29 09:26:46 +0800

    init

--- static/request/keepalive.js
+++ static/request/keepalive.js
@@ -4,17 +4,15 @@
  */
 function keepAlive(storage = window.storage, manageAPI = window.manageAPI) {
 
-    var timestamp = new Date().getTime();
-
-    var prestamp = storage.getStore(`keep_alive_status`)
+    const timestamp = new Date().getTime();
+    const prestamp = storage.getStore(`keep_alive_status`)
 
     //小时内,只执行一次keepalive
     if (prestamp != null && timestamp - prestamp > 60 * 60 * 1000) {
         storage.setStore(`keep_alive_status`, timestamp);
-
         //设置每十分钟请求用户信息
-        manageAPI.setTimeouts(() => {
-            manageAPI.queryToken();
+        Betools.manage.setTimeouts(() => {
+            Betools.manage.queryToken();
         }, 15 * 60 * 1000, 35 * 60 * 1000, 55 * 60 * 1000);
     }
 
--- static/request/keepsleep.js
+++
@@ -1,9 +0,0 @@
-/**
- * @function JS Sleep休眠函数
- * @param {*} time 
- */
-const sleep = async(time = 1000) => {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-window.sleep = sleep;
