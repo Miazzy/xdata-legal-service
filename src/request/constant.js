f0eceee31bb0735cd2c4ee065faad81723ed5141
yunwisdom <qq695926920@163.com>
2021-04-29 14:03:55 +0800

    init

--- src/request/constant.js
+++ src/request/constant.js
@@ -1,4 +1,4 @@
-export const PDF_PREVIEW_URL = `${window.requestAPIConfig.domain}/jeecg-boot/generic/web/viewer.html?file=`;
+export const PDF_PREVIEW_URL = `/jeecg-boot/generic/web/viewer.html?file=`;
 export const OFFICE_PREVIEW_URL = 'https://fileview.shengtai.club/onlinePreview?officePreviewType=image&url=';
 export const OFFICE_PREVIEW_TYPE = ['doc', 'docx', 'xlsx', 'xls', 'ppt', 'pptx'];
 export const WORKFLOW_STATUS = { 1: '待提交', 2: '处理中', 3: '审核中', 4: '已完成', 5: '已知会' };
