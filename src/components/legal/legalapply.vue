66328aa6b7b54cc6eb507fe4b9f9cc5c75940d83
yunwisdom <qq695926920@163.com>
2021-04-30 14:52:14 +0800

    init

--- src/components/legal/legalapply.vue
+++ src/components/legal/legalapply.vue
@@ -25,7 +25,7 @@
             <!-- 案件申请 -->
             <div style="background-color:#f0f0f0;">
 
-              <div class="reward-apply-content" style="height:auto; background-color:#fefefe; margin-top:0px; margin-left: 5rem; margin-right: 5rem; margin-bottom: 5rem; border: 1px solid #f0f0f0; front-size: 1rem;" >
+              <div id="legal-apply-content" class="reward-apply-content" style="height:auto; background-color:#fefefe; margin-top:0px; margin-left: 5rem; margin-right: 5rem; margin-bottom: 5rem; border: 1px solid #f0f0f0; front-size: 1rem;" >
 
                 <div class="reward-apply-header" style="height:80px; width:100%; text-align:center; margin-top:20px; font-size: 1.5rem; ">
                   案件发起申请
@@ -1450,23 +1450,24 @@ export default {
                   const { legal } = this;
 
                   legal.id = id;
-                  debugger ;
-                  legal.zone = JSON.stringify(legal.zone);
-                  legal.caseType = JSON.stringify(legal.caseType);
+
+                  legal.zone = JSON.stringify(legal.zone); //进行序列化
+                  legal.caseType = JSON.stringify(legal.caseType); //进行序列化
 
                   const result = await Betools.manage.postTableData(this.tablename , this.legal); // 向表单提交form对象数据
                   
-                  if(result && result.error && result.error.errno){
-                      return await vant.Dialog.alert({
-                        title: '温馨提示',
-                        message: `系统错误，请联系管理人员，错误编码：[${result.error.code}]. `,
-                      });
+                  legal.zone = JSON.parse(legal.zone); //进行序列化
+                  legal.caseType = JSON.parse(legal.caseType); //进行序列化
+
+                  if(result && result.error && result.error.errno){ //提交数据如果出现错误，请提示错误信息
+                      return await vant.Dialog.alert({  title: '温馨提示',  message: `系统错误，请联系管理人员，错误编码：[${result.error.code}]. `, });
                   }
 
+                  this.$toast.success('保存案件发起申请成功！');
                   this.loading = false; //设置状态
                   this.readonly = true;
                   this.role = 'view';
-                  this.$toast.success('保存案件发起申请成功！');
+                  vant.Dialog.alert({  title: '温馨提示',  message: `保存案件发起申请成功！`, });
                }
           });
 
@@ -1474,19 +1475,15 @@ export default {
 
       // 审批人员添加函数
       async rewardApproveAdd(){
-
         if(!this.approve_userid){
           return this.$toast.success('请选择审批人员处下拉列表中的待选审批人员！');
         }
-
         const index = this.approve_executelist.findIndex( item => {
           return item.userid == this.approve_userid;
         })
-
         if(index >= 0){
           return this.$toast.success('该审批人员已经添加，请重新输入！');
         }
-
         try {
           const mobile = this.approve_mobile ? `${this.approve_mobile.slice(0,3)}****${this.approve_mobile.slice(-4)}` : '';
           const user = {key: this.approve_executelist.length + 1 , id:Betools.tools.queryUniqueID(),username:this.approve_username , userid: this.approve_userid , mobile , company: this.approve_company , department : this.approve_department , position : this.approve_position};
@@ -1499,7 +1496,6 @@ export default {
         } catch (error) {
           console.log(error);
         }
-
       },
 
   },
@@ -1508,32 +1504,4 @@ export default {
 <style scoped >
     @import "../../assets/css/reward.home.css";
     @import "../../assets/css/reward.apply.css";
-    .reward-apply-content-item {
-      padding-top:5px;
-      padding-bottom: 5px;
-    }
-    #reward-download-excel-button {
-        background-image: linear-gradient(to right, #f96033, red);
-        margin: 10px 10px 10px 10px;
-        padding: 1px 20px;
-        border-radius: 8px;
-        color: #f0f0f0;
-        font-size: 12px;
-        text-align: center;
-        vertical-align: middle;
-        height: 27px;
-        line-height: 27px;
-    }
-    #reward-items-download-excel-button {
-        background-image: linear-gradient(to right, #f96033, red);
-        margin: 10px 10px 10px 10px;
-        padding: 1px 20px;
-        border-radius: 8px;
-        color: #f0f0f0;
-        font-size: 12px;
-        text-align: center;
-        vertical-align: middle;
-        height: 27px;
-        line-height: 27px;
-    }
 </style>
