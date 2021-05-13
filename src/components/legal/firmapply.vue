adf42a8b853763ac1a8e2e707027f5edd313904f
yunwisdom <qq695926920@163.com>
2021-05-13 23:06:48 +0800

    init

--- src/components/legal/firmapply.vue
+++ src/components/legal/firmapply.vue
@@ -599,15 +599,12 @@ export default {
             title: "确认操作",
             content: "是否确认保存此律所录入申请单?",
             onOk: async() => {
-
                   const { legal } = this;
                   legal.id = id;
                   const result = await Betools.manage.postTableData(this.tablename , this.legal); // 向表单提交form对象数据
-                  
                   if(result && result.error && result.error.errno){ //提交数据如果出现错误，请提示错误信息
                       return await vant.Dialog.alert({  title: '温馨提示',  message: `系统错误，请联系管理人员，错误编码：[${result.error.code}]. `, });
                   }
-
                   this.$toast.success('律所录入申请发起成功！');
                   this.loading = false; //设置状态
                   this.readonly = true;
