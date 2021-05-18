6b06262a5e6498e4799ef52241587a9835ccca89
yunwisdom <qq695926920@163.com>
2021-05-18 15:29:34 +0800

    init

---
+++ src/components/legal/lawsuitlist.vue
@@ -0,0 +1,101 @@
+<template>
+  
+  <div id="reward-home">
+      <div style="background-color:#f0f0f0;">
+      <a-menu mode="horizontal">
+        <a-sub-menu>
+            <span slot="title" class="submenu-title-wrapper" >
+              <a-avatar :src="userinfo.avatar" style="margin-left:0.25rem; margin-right:0.55rem;" />
+              {{ usertitle }} 
+            </span>
+            <a-menu-item-group title="应用中心">
+            <a-menu-item key="setting:1" :to="`/legal/message`"  @click="redirectView('/legal/message')" >
+                审批
+            </a-menu-item>
+            <a-menu-item key="setting:2" :to="`/legal/workspace`" @click="redirectView('/legal/workspace')" >
+                工作台
+            </a-menu-item>
+            </a-menu-item-group>
+        </a-sub-menu>
+      </a-menu>
+
+      <a-row :gutter="24">
+        <keep-alive>
+        </keep-alive>
+      </a-row>
+    </div>
+  </div>
+</template>
+<script>
+import * as workconfig from '@/request/workconfig';
+
+export default {
+  mixins: [window.mixin],
+  data() {
+    return {
+      iswechat:false,
+      iswework:false,
+      pageName: "案件管理",
+      momentNewMsg: true,
+      activeTabKey: 3,
+      acceptType:'*/*',
+      uploadURL:'',
+      tablename:'bs_law_firm',
+      size: 0,
+      options:{},
+      legal:{},
+      data: [],
+      readonly: false,
+      userList:[],
+      selectedSheet: null,
+      sheetName: null,
+      sheets: [{ name: "Sheet1", data: [{}] }],
+      collection: [{ }],
+      userinfo: '',
+      usertitle:'',
+      statusType:{'valid':'有效','invalid':'删除'},
+      zoneType:{'领地集团总部':'领地集团总部','重庆区域':'重庆区域','两湖区域':'两湖区域','川北区域':'川北区域','成都区域':'成都区域','乐眉区域':'乐眉区域','中原区域':'中原区域','攀西区域':'攀西区域','新疆区域':'新疆区域','大湾区域':'大湾区域','北京区域':'北京区域'},
+    };
+  },
+  activated() {
+  },
+  mounted() {
+    this.queryInfo();
+  },
+  methods: {
+      moment,
+
+      // 企业微信登录处理函数
+      async  weworkLogin  (codeType = 'search', systemType = 'search')  {
+        const userinfo_work = await Betools.query.queryWeworkUser(codeType, systemType,'v5');
+        const userinfo = await Betools.storage.getStore('system_userinfo');
+        this.legal.create_by = (userinfo ? userinfo.realname || userinfo.name || userinfo.lastname : '');
+        this.usertitle = (userinfo && userinfo.parent_company && userinfo.parent_company.name ? userinfo.parent_company.name + ' > ' :'')  + (userinfo ? userinfo.realname || userinfo.name || userinfo.lastname : '');
+        return userinfo;
+      },
+
+      // 执行页面跳转
+      async redirectView(path) {
+          this.$router.push(path);
+      },
+     
+      // 获取基础信息
+      async queryInfo() {
+        try {
+          this.iswechat = Betools.tools.isWechat(); //查询当前是否微信端
+          this.iswework = Betools.tools.isWework(); //查询是否为企业微信
+          this.userinfo = await this.weworkLogin(); //查询当前登录用户
+          this.back = Betools.tools.getUrlParam('back') || '/legal/workspace'; //查询上一页
+          const userinfo = await Betools.storage.getStore('system_userinfo');  //获取用户基础信息
+        } catch (error) {
+          console.log(error);
+        }
+      },
+
+  },
+};
+</script>
+<style scoped >
+    @import "../../assets/css/reward.home.css";
+    @import "../../assets/css/reward.apply.css";
+</style>
