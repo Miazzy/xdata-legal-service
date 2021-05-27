c0ce948955e4910aef2de8fe8594098823afebed
yunwisdom <qq695926920@163.com>
2021-05-27 22:21:02 +0800

    init

--- src/components/legal/case/legalapply.vue
+++ src/components/legal/case/legalapply.vue
@@ -1404,21 +1404,22 @@ export default {
             title: "确认操作",
             content: "是否确认保存此案件发起申请单?",
             onOk: async() => {
-
                   const { legal } = this;
                   legal.id = id;
                   legal.zone = JSON.stringify(legal.zone); //进行序列化
                   legal.caseType = JSON.stringify(legal.caseType); //进行序列化
                   legal.court = JSON.stringify(legal.court); //进行序列化
-                  const result = await Betools.manage.postTableData(this.tablename , legal); // 向表单提交form对象数据
+                  try { // TRY CATCH 不要移除，如果报错可能导致异常
+                    const result = await Betools.manage.postTableData(this.tablename , legal); // 向表单提交form对象数据
+                  } catch (error) {
+                    console.error(error);
+                  }
                   legal.zone = JSON.parse(legal.zone); //进行序列化
                   legal.caseType = JSON.parse(legal.caseType); //进行序列化
                   legal.court = JSON.parse(legal.court); //进行序列化
-                  
                   if(result && result.error && result.error.errno){ //提交数据如果出现错误，请提示错误信息
                       return await vant.Dialog.alert({  title: '温馨提示',  message: `系统错误，请联系管理人员，错误编码：[${result.error.code}]. `, });
                   }
-
                   this.loading = false; //设置状态
                   this.readonly = true;
                   this.role = 'view';
